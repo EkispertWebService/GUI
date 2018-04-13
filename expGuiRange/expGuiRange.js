97531b9483833aef09870851e30941cd1e407415
HikaruMaruyama <maruyama@val.co.jp>
2018-04-13 14:36:53 +0900

    新しい範囲探索のサンプルコードです。

--- expGuiRange/expGuiRange.js
+++ expGuiRange/expGuiRange.js
@@ -4,7 +4,7 @@
  *  サンプルコード
  *  https://github.com/EkispertWebService/GUI
  *  
- *  Version:2016-06-20
+ *  Version:2017-11-15
  *  
  *  Copyright (C) Val Laboratory Corporation. All rights reserved.
  **/
@@ -23,10 +23,10 @@ var expGuiRange = function (pObject, config) {
         if (s.src && s.src.match(/expGuiRange\.js(\?.*)?/)) {
             var params = s.src.replace(/.+\?/, '');
             params = params.split("&");
-            for (var i = 0; i < params.length; i++) {
-                var tmp = params[i].split("=");
+            for (var j = 0; j < params.length; j++) {
+                var tmp = params[j].split("=");
                 if (tmp[0] == "key") {
-                    key = unescape(tmp[1]);
+                    key = decodeURIComponent(tmp[1]);
                 }
             }
             break;
@@ -35,14 +35,15 @@ var expGuiRange = function (pObject, config) {
 
     // 変数郡
     var stationList = new Array();
+    var baseStationList = new Array();
     var httpObj;
     // 設定
     var transferCount;
     var callbackFunction; // コールバック関数の設定
 
     /**
-    * 駅名の検索
-    */
+     * 駅名の検索
+     */
     function searchStation(station, upperLimit, callback) {
         if (typeof httpObj != 'undefined') {
             httpObj.abort();
@@ -56,7 +57,7 @@ var expGuiRange = function (pObject, config) {
             url += "&code=" + station;
         }
         url += "&upperLimit=" + upperLimit;
-        // 都道府県
+        // 乗り換え回数上限
         if (typeof transferCount != 'undefined') {
             url += "&transferCount=" + transferCount;
         }
@@ -99,25 +100,96 @@ var expGuiRange = function (pObject, config) {
     }
 
     /**
-    * JSONを解析してリストをセット
-    */
+     * 駅名の検索
+     */
+    function searchMultipleStation(station, upperMinute, callback) {
+        if (typeof httpObj != 'undefined') {
+            httpObj.abort();
+        }
+        var url = apiURL + "v1/json/search/multipleRange?key=" + key;
+        url += "&baseList=" + arrayURIComponent(station.split(":")).join(":");
+        url += "&upperMinute=" + upperMinute;
+        // 乗り換え回数上限
+        if (typeof transferCount != 'undefined') {
+            url += "&upperTransferCount=" + transferCount;
+        }
+        // コールバック
+        callbackFunction = callback;
+        // 駅リスト初期化
+        stationList = new Array();
+        // リクエスト
+        var JSON_object = {};
+        if (window.XDomainRequest) {
+            // IE用
+            httpObj = new XDomainRequest();
+            httpObj.onload = function () {
+                JSON_object = JSON.parse(httpObj.responseText);
+                splitStation(JSON_object);
+            };
+            httpObj.onerror = function () {
+                // エラー時の処理
+                if (typeof callbackFunction == 'function') {
+                    callbackFunction(false);
+                }
+            };
+        } else {
+            httpObj = new XMLHttpRequest();
+            httpObj.onreadystatechange = function () {
+                var done = 4, ok = 200;
+                if (httpObj.readyState == done && httpObj.status == ok) {
+                    JSON_object = JSON.parse(httpObj.responseText);
+                    splitStation(JSON_object);
+                } else if (httpObj.readyState == done && httpObj.status != ok) {
+                    // エラー時の処理
+                    if (typeof callbackFunction == 'function') {
+                        callbackFunction(false);
+                    }
+                }
+            };
+        }
+        httpObj.open("GET", url, true);
+        httpObj.send(null);
+    }
+
+    /**
+     * 配列をURLエンコードする
+     */
+    function arrayURIComponent(arr) {
+        var ret = new Array();
+        for (var i = 0; i < arr.length; i++) {
+            ret.push(encodeURIComponent(arr[i]));
+        }
+        return ret;
+    }
+
+    /**
+     * JSONを解析してリストをセット
+     */
     function splitStation(pointObject) {
         if (typeof pointObject.ResultSet.Point == 'undefined') {
             // 失敗
             if (typeof callbackFunction == 'function') {
                 callbackFunction(false);
             }
-        } else if (typeof pointObject.ResultSet.Point.length == 'undefined') {
-            stationList.push(pointObject.ResultSet.Point);
-            // 成功
-            if (typeof callbackFunction == 'function') {
-                callbackFunction(true);
-            }
         } else {
-            for (var i = 0; i < pointObject.ResultSet.Point.length; i++) {
-                stationList.push(pointObject.ResultSet.Point[i]);
+            // 範囲探索の結果を取得
+            if (typeof pointObject.ResultSet.Point.length == 'undefined') {
+                stationList.push(pointObject.ResultSet.Point);
+            } else {
+                for (var i = 0; i < pointObject.ResultSet.Point.length; i++) {
+                    stationList.push(pointObject.ResultSet.Point[i]);
+                }
             }
-            // 成功
+            // ベースとなる駅のリスト取得
+            if (typeof pointObject.ResultSet.Base != 'undefined') {
+                if (typeof pointObject.ResultSet.Base.length == 'undefined') {
+                    baseStationList.push(pointObject.ResultSet.Base);
+                } else {
+                    for (var i = 0; i < pointObject.ResultSet.Base.length; i++) {
+                        baseStationList.push(pointObject.ResultSet.Base[i]);
+                    }
+                }
+            }
             if (typeof callbackFunction == 'function') {
                 callbackFunction(true);
             }
@@ -125,8 +197,8 @@ var expGuiRange = function (pObject, config) {
     }
 
     /**
-    * 駅データのオブジェクトを作成
-    */
+     * 駅データのオブジェクトを作成
+     */
     function createStationObject(stationObj) {
         var tmp_station = new Object();
         tmp_station.name = stationObj.Station.Name;
@@ -144,14 +216,37 @@ var expGuiRange = function (pObject, config) {
             tmp_station.type = stationObj.Station.Type;
         }
         tmp_station.kenCode = stationObj.Prefecture.code;
-        tmp_station.minute = Number(stationObj.Minute);
+        if (typeof stationObj.Minute != 'undefined') {
+            tmp_station.minute = Number(stationObj.Minute);
+        }
+        if (typeof stationObj.TransferCount != 'undefined') {
+            tmp_station.transferCount = Number(stationObj.TransferCount);
+        }
+        if (typeof stationObj.Cost != 'undefined') {
+            if (typeof stationObj.Cost.length == 'undefined') {
+                tmp_station.baseStation = baseStationList[parseInt(stationObj.Cost.baseIndex) - 1].Point.Station.Name;
+                tmp_station.minute = Number(stationObj.Cost.Minute);
+                tmp_station.transferCount = Number(stationObj.Cost.TransferCount);
+            } else {
+                var tmp_baseStationList = new Array();
+                var tmp_minuteList = new Array();
+                var tmp_transferCountList = new Array();
+                for (var i = 0; i < stationObj.Cost.length; i++) {
+                    tmp_baseStationList.push(baseStationList[parseInt(stationObj.Cost[i].baseIndex) - 1].Point.Station.Name);
+                    tmp_minuteList.push(Number(stationObj.Cost[i].Minute));
+                    tmp_transferCountList.push(Number(stationObj.Cost[i].TransferCount));
+                }
+                tmp_station.baseStation = tmp_baseStationList.join(",");
+                tmp_station.minute = tmp_minuteList.join(",");
+                tmp_station.transferCount = tmp_transferCountList.join(",");
+            }
+        }
         return tmp_station;
     }
 
-
     /**
-    * 駅一覧の取得
-    */
+     * 駅一覧の取得
+     */
     function getStationList() {
         var tmpStationName = new Array();
         for (var i = 0; i < stationList.length; i++) {
@@ -161,8 +256,8 @@ var expGuiRange = function (pObject, config) {
     }
 
     /**
-    * 時間から地点オブジェクトを取得
-    */
+     * 時間から地点オブジェクトを取得
+     */
     function getPointObject(name) {
         for (var i = 0; i < stationList.length; i++) {
             if (stationList[i].Station.Name == name) {
@@ -172,12 +267,17 @@ var expGuiRange = function (pObject, config) {
         return;
     }
 
+    /**
+     * 環境設定
+     */
     function setConfigure(name, value) {
         if (name.toLowerCase() == String("apiURL").toLowerCase()) {
             apiURL = value;
         } else if (name.toLowerCase() == String("key").toLowerCase()) {
             key = value;
         } else if (name == "transferCount") {
+            transferCount = value;
+        } else if (name == "upperTransferCount") {
             transferCount = value;
         } else if (String(name).toLowerCase() == String("ssl").toLowerCase()) {
             if (String(value).toLowerCase() == "true" || String(value).toLowerCase() == "enable" || String(value).toLowerCase() == "enabled") {
@@ -187,8 +287,10 @@ var expGuiRange = function (pObject, config) {
             }
         }
     }
+
     // 外部参照可能な関数リスト
     this.searchStation = searchStation;
+    this.searchMultipleStation = searchMultipleStation;
     this.getStationList = getStationList;
     this.getPointObject = getPointObject;
     this.setConfigure = setConfigure;
