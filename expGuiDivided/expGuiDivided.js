41ac8ebe6b3744ccfb6f8ba498da52060df77615
hashimoto <hashimoto@val.co.jp>
2018-10-15 19:02:06 +0900

    ローカル変数名を修正。

--- expGuiDivided/expGuiDivided.js
+++ expGuiDivided/expGuiDivided.js
@@ -23,8 +23,8 @@ var expGuiDivided = function (pObject, config) {
         if (s.src && s.src.match(/expGuiDivided\.js(\?.*)?/)) {
             var params = s.src.replace(/.+\?/, '');
             params = params.split("&");
-            for (var i = 0; i < params.length; i++) {
-                var tmp = params[i].split("=");
+            for (var j = 0; j < params.length; j++) {
+                var tmp = params[j].split("=");
                 if (tmp[0] == "key") {
                     key = unescape(tmp[1]);
                 }
