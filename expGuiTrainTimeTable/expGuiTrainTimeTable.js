cf9cc698aab8f4a0f21be12da473554a7863698a
hashimoto <hashimoto@val.co.jp>
2018-10-15 19:02:06 +0900

    ローカル変数名を修正。

--- expGuiTrainTimeTable/expGuiTrainTimeTable.js
+++ expGuiTrainTimeTable/expGuiTrainTimeTable.js
@@ -27,8 +27,8 @@ var expGuiTrainTimeTable = function (pObject, config) {
         if (s.src && s.src.match(/expGuiTrainTimeTable\.js(\?.*)?/)) {
             var params = s.src.replace(/.+\?/, '');
             params = params.split("&");
-            for (var i = 0; i < params.length; i++) {
-                var tmp = params[i].split("=");
+            for (var j = 0; j < params.length; j++) {
+                var tmp = params[j].split("=");
                 if (tmp[0] == "key") {
                     key = unescape(tmp[1]);
                 }
