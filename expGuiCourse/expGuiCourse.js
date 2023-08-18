94f40d19e22b8efa6993b4c4882709237ffe2542
hashimoto <hashimoto@val.co.jp>
2023-08-18 16:39:29 +0900

    経路表示パーツのバージョンを更新。

--- expGuiCourse/expGuiCourse.js
+++ expGuiCourse/expGuiCourse.js
@@ -4,7 +4,7 @@
  *  サンプルコード
  *  https://github.com/EkispertWebService/GUI
  *
- *  Version:2022-02-17
+ *  Version:2023-08-25
  *
  *  Copyright (C) Val Laboratory Corporation. All rights reserved.
  **/
