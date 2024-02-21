568eaa342914151f903ef55f00bbc22fe654002e
takashiyamazaki <takashiyamazaki@val.co.jp>
2024-02-21 09:43:45 +0900

    旧名称（駅すぱあとWebサービス、駅すぱあと Web サービス、Webサービス）を「駅すぱあと API」に変更

--- expGuiDivided/expGuiDivided.js
+++ expGuiDivided/expGuiDivided.js
@@ -1,5 +1,5 @@
 /**
- *  駅すぱあと Web サービス
+ *  駅すぱあと API
  *  分割計算パーツ
  *  サンプルコード
  *  https://github.com/EkispertWebService/GUI
@@ -10,7 +10,7 @@
  **/
 
 var expGuiDivided = function (pObject, config) {
-    // Webサービスの設定
+    // 駅すぱあと APIの設定
     var apiURL = "http://api.ekispert.jp/";
 
     // GETパラメータからキーの設定
--- expGuiSectionTimeTable/expGuiSectionTimeTable.js
+++ expGuiSectionTimeTable/expGuiSectionTimeTable.js
@@ -1,5 +1,5 @@
 /**
- *  駅すぱあと Web サービス
+ *  駅すぱあと API
  *  区間時刻表パーツ
  *  サンプルコード
  *  https://github.com/EkispertWebService/GUI
@@ -14,7 +14,7 @@ var expGuiSectionTimeTable = function (pObject, config) {
     var documentObject = pObject;
     var baseId = pObject.id;
 
-    // Webサービスの設定
+    // 駅すぱあと APIの設定
     var apiURL = "http://api.ekispert.jp/";
 
     // GETパラメータからキーの設定
